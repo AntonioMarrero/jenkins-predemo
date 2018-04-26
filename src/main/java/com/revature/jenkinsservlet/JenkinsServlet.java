bd75f4ed6c1af67003ed996e872c8314fcf8385b
Antonio Marrero Bonilla <37803934+AntonioMarrero@users.noreply.github.com>
2018-04-26 14:02:33 -0400

    Add files via upload

---
+++ src/main/java/com/revature/jenkinsservlet/JenkinsServlet.java
@@ -0,0 +1,41 @@
+package com.revature.jenkinsservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JenkinsServlet
+ */
+public class JenkinsServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public JenkinsServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.write("Application successfully built using Jenkins, and deployed to an AWS EC2 instance!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
